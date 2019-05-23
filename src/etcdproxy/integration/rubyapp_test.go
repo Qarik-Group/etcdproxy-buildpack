cd39ba1a151548db579e41d35c7f6db3e90da398
Dr Nic Williams <drnicwilliams@gmail.com>
2019-05-23 12:48:56 +1000

    simple integration test to ensure rubyapp is running

---
+++ src/etcdproxy/integration/rubyapp_test.go
@@ -0,0 +1,27 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Rubyapp Integration Test", func() {
+	var app *cutlass.App
+	AfterEach(func() {
+		if app != nil {
+			app.Destroy()
+		}
+		app = nil
+	})
+
+	It("app deploys", func() {
+		app = cutlass.New(filepath.Join(bpDir, "fixtures", "rubyapp"))
+		app.Manifest = filepath.Join(bpDir, "fixtures", "rubyapp", "manifest.cfdev.yml")
+		V3PushAppAndConfirm(app)
+		Expect(app.GetBody("/")).To(ContainSubstring("Hi, I'm an app with an etcd grpc-proxy sidecar!"))
+	})
+})
